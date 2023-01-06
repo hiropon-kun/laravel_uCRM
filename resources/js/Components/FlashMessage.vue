436fb256213af316427f89eec7ad99f467e9bb42
hiropon-kun <gudorobetta@yahoo.co.jp>
2023-01-06 09:34:42 +0900

    商品関連

---
+++ resources/js/Components/FlashMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v-if="$page.props.flash.status === 'success'" class="bg-blue-300 text-white p-4">
+      {{ $page.props.flash.message }}
+    </div>
+    <div v-if="$page.props.flash.status === 'danger'" class="bg-red-300 text-white p-4">
+      {{ $page.props.flash.message }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
